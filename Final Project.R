c6fcae60e2f6daca78836ff48a0dfb8a59a27b35
viniciushurtado <32720818+viniciushurtado@users.noreply.github.com>
2017-11-03 03:54:39 -0200

    updated dataset

--- Final Project.R
+++ Final Project.R
@@ -2,7 +2,4 @@ library(utils)
 library(ggplot2)
 library(caret)
 
-Cleveland = read.csv("processed.cleveland.data",header=FALSE)
-Switzerland = read.csv("processed.switzerland.data",header=FALSE)
-Va = read.csv("processed.va.data",header=FALSE)
-Hungarian = read.csv("processed.hungarian.data",header=FALSE)
+Cleveland = read.csv("processed.cleveland.csv",header=FALSE)
