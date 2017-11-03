b0d961ee912130c75ffe520fc42bd8fb15f2da97
viniciushurtado <32720818+viniciushurtado@users.noreply.github.com>
2017-11-03 03:52:11 -0200

    deleted setwd

--- Final Project.R
+++ Final Project.R
@@ -2,8 +2,6 @@ library(utils)
 library(ggplot2)
 library(caret)
 
-setwd("~/Stanford/Courses/Data Science/Final Work/DataSets")
-
 Cleveland = read.csv("processed.cleveland.data",header=FALSE)
 Switzerland = read.csv("processed.switzerland.data",header=FALSE)
 Va = read.csv("processed.va.data",header=FALSE)
