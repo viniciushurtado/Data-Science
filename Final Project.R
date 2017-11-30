da735a79eb0f351f3be31974ffe4bbba4deeefd2
viniciushurtado <32720818+viniciushurtado@users.noreply.github.com>
2017-11-30 05:40:43 -0200

    project update

--- Final Project.R
+++ Final Project.R
@@ -23,9 +23,10 @@
 
 library(utils)
 library(caret)
-featureselection = 0 # change to 1 to use feature selection
+library(corrplot)
+library(dplyr)
 
-#Step 1: Import Data----
+#Step 1: Import data----
 Cleveland = read.csv("processed.cleveland.csv",header=FALSE) #cleveland dataset impor
 colnames(Cleveland) = c("age","sex","cp","trestbps","chol","fbs",
                         "restecg","thalach","exang","oldpeak","slope","ca","thal","num") #rename columns
@@ -46,46 +47,7 @@ Cleveland$slope = as.factor(as.character(Cleveland$slope))
 #create a table with numeric variables (will be use for correlogram visualization)
 Correlogram = Cleveland[,c(1,4,5,8,10)]
 
-#Step 2: Feature Selection (if not running with feature selection, there is no need to run step 2)----
-
-old = Sys.time() #measure runtime
-#initialfeature=sample(seq(1,13,1),1) #selecting an initial feature do run model
-initialfeature=3 #I tried to used this one to start
-
-selectedfeatures=c(initialfeature,14) #define first column to predict num2
-possiblefeatures=seq(1,ncol(Cleveland)-1,1) #define other columns as possible features
-
-training=Cleveland[seq(1,nrow(Cleveland),2),selectedfeatures] #define the inicial training set
-testing=Cleveland[seq(2,nrow(Cleveland),2),selectedfeatures] #define the inicial testing set
-model = train(as.character(num2) ~.,training,method="kknn") #train model
-
-predictedNum = predict(model,testing) #test model
-base_accuracy = sum(predictedNum==as.character(testing$num2))/nrow(testing) #define base acc
-
-(new = Sys.time() - old) #measure runtime
-print(base_accuracy)
-
-#feature selection loop
-for (i in 1:length(possiblefeatures)){ #features add loop for all possible features
-  if (i!=initialfeature){ #exclude initial feature from loop
-    training=Cleveland[seq(1,nrow(Cleveland),2),c(selectedfeatures,possiblefeatures[i])] #define the inicial training set
-    testing=Cleveland[seq(2,nrow(Cleveland),2),c(selectedfeatures,possiblefeatures[i])] #define the inicial testing set
-    model = train(as.character(num2) ~.,training,method="kknn") #train new model
-    predictedNum = predict(model,testing) #test new model
-    accuracy = sum(predictedNum==as.character(testing$num2))/nrow(testing) #measure new accuracy
-    
-    if (accuracy>base_accuracy){ #replace base accuracy if new test is better
-      selectedfeatures=c(selectedfeatures,possiblefeatures[i])
-      base_accuracy=accuracy
-    }
-  }
-  (new = Sys.time() - old) #measure runtime
-  print(i)
-  print(selectedfeatures)
-  print(base_accuracy)
-}
-
-#Step 3: Heart Disease Classification----
+#Step 2: Best model Selection----
 
 if (featureselection==1){ #if not using feature selection, use all features
     Cleveland = Cleveland[,c(selectedfeatures)]
@@ -100,35 +62,61 @@ m5 = train(as.character(num2)~.,data=Cleveland, trControl=train_control, method=
 
 allModels=resamples(list(RandomForest=m1,LogitBoost=m2,KKNN=m3,NeuralNetwork=m4,SVM=m5)) #label the model to compare
 bwplot(allModels,scales=list(relation="free"))
+varImp(m1)
+m1$results
 
-#Step 4: Dataset visualization----
-ggplot(Cleveland,aes(num2))+geom_bar() #count of Heart Disease Diagnosis (>0)
+bestmodel = "rf" #input of the best model based on Step 2
 
-#boxplot for numeric variables vs num2: 1=age,4=trestbps,5=chol,8=thalach,10=oldpeak
-ggplot(Cleveland,aes(as.character(num2),age))+geom_boxplot() #visual relation to num2
-ggplot(Cleveland,aes(as.character(num2),trestbps))+geom_boxplot()
-ggplot(Cleveland,aes(as.character(num2),chol))+geom_boxplot()
-ggplot(Cleveland,aes(as.character(num2),thalach))+geom_boxplot() #visual relation to num2
-ggplot(Cleveland,aes(as.character(num2),oldpeak))+geom_boxplot() #visual relation to num2
+#Step 3: Greedy Feature Selection----
 
-#Jitter plot for discrete variables vs num2: 2=sex,3=cp,6=fbs,7=restecg,9=exang,11=slope,12=ca,13=thal
-ggplot(Cleveland,aes(num2,sex))+geom_jitter() #visual relation to num2
-ggplot(Cleveland,aes(num2,cp))+geom_jitter() #visual relation to num2
-ggplot(Cleveland,aes(num2,fbs))+geom_jitter() 
-ggplot(Cleveland,aes(num2,restecg))+geom_jitter()
-ggplot(Cleveland,aes(num2,exang))+geom_jitter() #visual relation to num2
-ggplot(Cleveland,aes(num2,slope))+geom_jitter() #visual relation to num2
-ggplot(Cleveland,aes(num2,ca))+geom_jitter() #visual relation to num2
-ggplot(Cleveland,aes(num2,thal))+geom_jitter() #select
+old = Sys.time() #measure runtime
+initialfeature=sample(seq(1,13,1),1) #selecting an initial feature do run model
+#initialfeature=8 #I tried to used this one to start
 
-#features that influence num2 based on visualization = 1,8,10,2,3,9,11,12,13
-#We could use those features to test the model and compare the result
-#in my tests with those features the result was better than greedy feature solution
+selectedfeatures=c(initialfeature,14) #define first column to predict num2
+possiblefeatures=seq(1,ncol(Cleveland)-1,1) #define other columns as possible features
+
+training=Cleveland[seq(1,nrow(Cleveland),2),selectedfeatures] #define the inicial training set
+testing=Cleveland[seq(2,nrow(Cleveland),2),selectedfeatures] #define the inicial testing set
+model = train(as.character(num2) ~.,training,method=bestmodel) #train model
+
+predictedNum = predict(model,testing) #test model
+base_accuracy = sum(predictedNum==as.character(testing$num2))/nrow(testing) #define base acc
+
+(new = Sys.time() - old) #measure runtime
+print(base_accuracy)
+
+#feature selection loop
+for (i in 1:length(possiblefeatures)){ #features add loop for all possible features
+  if (i!=initialfeature){ #exclude initial feature from loop
+    training=Cleveland[seq(1,nrow(Cleveland),2),c(selectedfeatures,possiblefeatures[i])] #define the inicial training set
+    testing=Cleveland[seq(2,nrow(Cleveland),2),c(selectedfeatures,possiblefeatures[i])] #define the inicial testing set
+    model = train(as.character(num2) ~.,training,method=bestmodel) #train new model
+    predictedNum = predict(model,testing) #test new model
+    accuracy = sum(predictedNum==as.character(testing$num2))/nrow(testing) #measure new accuracy
+    
+    if (accuracy>base_accuracy){ #replace base accuracy if new test is better
+      selectedfeatures=c(selectedfeatures,possiblefeatures[i])
+      base_accuracy=accuracy
+    }
+  }
+  (new = Sys.time() - old) #measure runtime
+  print(i)
+  print(selectedfeatures)
+  print(base_accuracy)
+}
+
+#step 4: Final model run----
+train_control = trainControl(method="repeatedcv", number = 10, repeats = 3)
+mbest = train(as.character(num2)~.,data=Cleveland, trControl=train_control, method=bestmodel)
+varImp(mbest)
+mbest$result
+
+#Step 5: Dataset visualization----
 
 #Correlogram between numeric variables - to investigate if there is correlation between numeric variables
-library(corrplot)
 M = cor(Correlogram)
 corrplot(M, method = "circle")
 
-varImp(m4) #show variables importance for model
-m4$results #show model results
+ggplot(Cleveland,aes(x=oldpeak,y=thalach,color=as.character(num2)))+geom_point(size=3)
+ggplot(filter(Cleveland, thal=="3.0", ca=="0.0"),aes(x=oldpeak,y=thalach,color=as.character(num2)))+geom_point(size=3)
